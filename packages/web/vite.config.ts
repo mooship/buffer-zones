84011e5ce61de876192f903677b9c705da25b1a2
Timothy Brits <timothybrits@icloud.com>
2026-07-31 09:18:31 +0200

    build(web): add Vite caching and explicit chunk splitting

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,6 +5,7 @@ import { FontaineTransform } from "fontaine";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  cacheDir: "node_modules/.vite",
   plugins: [
     react(),
     babel({
@@ -17,6 +18,46 @@ export default defineConfig({
   ],
   build: {
     assetsInlineLimit: 0,
+    cssCodeSplit: true,
+    rolldownOptions: {
+      output: {
+        entryFileNames: "assets/js/[name]-[hash].js",
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: (assetInfo) => {
+          const firstName = assetInfo.names?.[0] ?? assetInfo.name ?? "";
+
+          if (/\.css$/i.test(firstName)) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          if (/\.(woff2?|ttf|otf|eot)$/i.test(firstName)) {
+            return "assets/fonts/[name]-[hash][extname]";
+          }
+
+          if (/\.(png|jpe?g|gif|svg|webp|avif|ico)$/i.test(firstName)) {
+            return "assets/img/[name]-[hash][extname]";
+          }
+
+          return "assets/misc/[name]-[hash][extname]";
+        },
+        codeSplitting: {
+          groups: [
+            {
+              name: "react-vendor",
+              test: /node_modules\/(react|react-dom|scheduler)\//,
+            },
+            {
+              name: "map-vendor",
+              test: /node_modules\/(leaflet|react-leaflet)\//,
+            },
+            {
+              name: "ui-vendor",
+              test: /node_modules\/(lucide-react|zustand|usehooks-ts|zod)\//,
+            },
+          ],
+        },
+      },
+    },
   },
   test: {
     environment: "happy-dom",
